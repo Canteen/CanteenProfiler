27525bdfb7020fdc06d9dc2cd8a7f26aebdcb1a0
Matt Karl <matt@mattkarl.com>
2014-01-26 22:40:47 -0500

    Auto updated docs

---
+++ api.js
@@ -0,0 +1,20 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Profiler",
+        "ProfilerGhostNode",
+        "ProfilerNode",
+        "ProfilerRenderer",
+        "ProfilerSQLNode"
+    ],
+    "modules": [
+        "Canteen_Profiler"
+    ],
+    "allModules": [
+        {
+            "displayName": "Canteen\\Profiler",
+            "name": "Canteen_Profiler"
+        }
+    ]
+} };
+});
